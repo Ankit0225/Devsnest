7ec9637594971189f562237bea21ca5e4158f056
Ankit <ankitmaurya0225@gmail.com>
2021-06-18 09:29:48 +0530

    Take Home Assignment 6

---
+++ Devsnest Frontend/Take Home Assignment 6/script 1.js
@@ -0,0 +1,7 @@
+function is_array(e){
+    return Array.isArray(e);
+
+}
+console.log(is_array('w3resource'));
+console.log(is_array([1,2,4,0]));
+
---
+++ Devsnest Frontend/Take Home Assignment 6/script 2.js
@@ -0,0 +1,2 @@
+let array_clone = [1,2,4,0]
+console.log(array_clone)
